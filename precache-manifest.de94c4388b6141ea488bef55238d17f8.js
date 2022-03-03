9015935951cba6bacecf15eb1da564d2bfc0de10
Mk13Ghoul <mayankkumarj5@gmail.com>
2022-03-03 23:32:15 +0530

    Updates

---
+++ precache-manifest.de94c4388b6141ea488bef55238d17f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fc19cd30ca99249e433afddc9d3757",
+    "url": "/Covid19-Tracker/index.html"
+  },
+  {
+    "revision": "a42b77865cb2f54691ae",
+    "url": "/Covid19-Tracker/static/css/2.7c35e809.chunk.css"
+  },
+  {
+    "revision": "b022e16d7986edd77a2b",
+    "url": "/Covid19-Tracker/static/css/main.54d43338.chunk.css"
+  },
+  {
+    "revision": "a42b77865cb2f54691ae",
+    "url": "/Covid19-Tracker/static/js/2.ac5b472d.chunk.js"
+  },
+  {
+    "revision": "68eb484dce045119a52132a8eb902e52",
+    "url": "/Covid19-Tracker/static/js/2.ac5b472d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b022e16d7986edd77a2b",
+    "url": "/Covid19-Tracker/static/js/main.8432d5a0.chunk.js"
+  },
+  {
+    "revision": "581cd3a6352e65602016",
+    "url": "/Covid19-Tracker/static/js/runtime-main.e42b7ca7.js"
+  }
+]);
